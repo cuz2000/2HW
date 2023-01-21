6446863ba701c81581642e81b149045d8c4e96ae
Your Name <you@example.com>
2023-01-22 01:25:30 +0300

    Добавлено решение 1й задачи

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine ("Введите трехзначное число");
+
+int num = Convert.ToInt32 ( Console.ReadLine());
+
+if (99 < num && num < 1000)
+{
+    
+    string stringNumber = Convert.ToString(num);
+    Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+}
+
+else
+    Console.WriteLine (" Вы ввели некорректное число. Введите трёхзначное число.");
